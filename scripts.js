1649461b0085753f9b06f5ffb46b40c917dd57b2
Lucas Gomes <luxcas.gomes@gmail.com>
2021-05-18 13:45:30 -0300

    Jogo da velha implementado
    Acrescentei um jogo da velha simples ao desafio.

--- scripts.js
+++ scripts.js
@@ -48,5 +48,112 @@ function contar(){
         res.innerHTML+= num[c]+", "
     }
 
-} 
+}
+
+const celulas = document.querySelectorAll(".casa")
+
+let checarTurno = true;
+
+const Player1 = "X"
+const Player2 = "O"
+
+const vitoria = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
+
+addEventListener("click", (event)=>{
+    if(event.target.matches(".casa")){
+
+        const posicoesDisponiveis = [];
+        for (index in celulas){
+            if(!isNaN(index)){
+                if(
+                    !celulas[index].classList.contains("X")
+                    &&
+                    !celulas[index].classList.contains("O")
+                ){
+                    jogar(event.target.id)                    
+                }
+            }
+        }
+
+        checarVencedor(turno);
+    }
+
+})
+
+function jogar(id){
+    const casa = document.getElementById(id)
+    turno = checarTurno ? Player1 : Player2;
+
+    casa.textContent = turno;
+    casa.classList.add(turno)
+}
+
+function checarVencedor(turno){
+    const vencedor = vitoria.some((comb)=>{
+        return comb.every((index)=>{
+            return celulas[index].classList.contains(turno);
+        })
+    })
+
+    if(vencedor){
+        fimDeJogo(turno)
+    } else if (checarEmpate()){
+        fimDeJogo()
+    } else{        
+    checarTurno = !checarTurno
+    }
+}
+
+function checarEmpate(){
+    let X = 0
+    let O = 0
+
+    for(index in celulas){
+        if(!isNaN(index)){
+            if(celulas[index].classList.contains(Player1)){
+                X++
+            } 
+            if(celulas[index].classList.contains(Player2)){
+                O++
+            }
+        }
+        
+    }
+    
+    return X + O === 9 ? true : false;
+}
+
+function fimDeJogo(vencedor = null){
+    const telaEscura = document.getElementById("tela-escura")
+
+    const h2 = document.createElement("h2")
+    const h3 = document.createElement("h3")
+
+
+    telaEscura.style.display="block"
+    telaEscura.appendChild(h2)
+    telaEscura.appendChild(h3)
+
+    if(vencedor){
+        h2.innerHTML=`O Jogador <span>${vencedor}</span> venceu`
+    } else{
+        h2.innerHTML=`Empatou`
+    }
+
+    let contador = 3;
+    setInterval(()=>{
+        h3.innerHTML=`Reiniciando em ${contador--}`
+    }, 1000)
+
+    setTimeout(()=> location.reload(),4000)
+}
 
